a1f5faf589b924af141729cfe833ef41d68e20d4
madukaHasantha <sbmhsenavirathna.up@gmail.com>
2021-03-21 10:29:17 +0530

    done ui

--- app/src/main/java/com/example/panthiya/edit_assingment.java
+++ app/src/main/java/com/example/panthiya/edit_assingment.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.tables2);
         setContentView(R.layout.activity_tableone);
         setContentView(R.layout.activity_edit_assingment);
     }
